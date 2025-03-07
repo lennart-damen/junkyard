9ea7a3533675841490d098f30dae33b317d40a53
Lennart Damen <lennart.damen@hotmail.com>
2025-03-07 08:49:03 +0100

    Make the fastapi app cooler

--- fastapi-prometheus-metrics/app/main.py
+++ fastapi-prometheus-metrics/app/main.py
@@ -6,8 +6,28 @@ app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
 @app.get("/ping")
-def home():
+def ping():
+    return "pong"
+
+@app.get("/flaky")
+def flaky():
     if random.choice([True, False]):
-        return "pong"
+        return "you got lucky"
     else:
         raise HTTPException(status_code=404, detail="Not Found")
+
+@app.get("/bad-guy")
+def bad_guy():
+    raise HTTPException(status_code=500, detail="I'm a bad guy")
+
+@app.get("/do-something")
+def do_something():
+    for _ in range(1000):
+        random.random()
+    return "done, that was easy"
+
+@app.get("/do-a-lot")
+def do_a_lot():
+    for _ in range(99999999):
+        random.random()
+    return "done, but that was hard"
